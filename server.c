7ecde7aa313021cd69e16a8e321ddc44d79a3f9c
Tymoteusz Kupcewicz <t.kupcewicz@gmail.com>
2017-02-22 18:50:43 +0100

    Add player

--- server.c
+++ server.c
@@ -99,6 +99,9 @@ void process_msg(int id, char* msg, char * response){
         sprintf(response,"%s#%d#%d", "$CONNECTED", SEED, id);
         send(client_socket[id] , response , strlen(response) , 0 );
         sprintf(response,"%s#%d", "$ADD_PLAYER", id);
+        for(k = 0; k < MAX_CLIENTS; k++){
+            send(client_socket[k] , response , strlen(response) , 0 );
+        }
     }
     else if(strcmp(tokens[0],"READY") == 0){
         ready_clients[id] = 1;
