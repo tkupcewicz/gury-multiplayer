11558db555c74e9678184dfa2f3367bcb2c94f69
Tymoteusz Kupcewicz <t.kupcewicz@gmail.com>
2017-02-22 13:27:40 +0100

    Rewritten server
    Rewritten server to use select() on single thread

--- server.c
+++ server.c
@@ -1,101 +1,238 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <time.h>
-#include <pthread.h>
+#include <sys/time.h>
+  
+#define TRUE   1
+#define FALSE  0
+#define PORT 4444
+#define SEED 999999
 
-#define SERVER_PORT 1234
-#define QUEUE_SIZE 5
-#define MAX 80
+char* process_msg(int id, char* msg){
+    char **tokens;
+    tokens = str_split(msg, '#');
+    if(strcmp(tokens[0],"NEW_GAME") == 0){
 
-void func(int sockfd)
-{
-  char buff[MAX];
-  int n;
-  for(;;){
-
-    bzero(buff,MAX);
-    read(sockfd,buff,sizeof(buff));
-    printf("From client: %s\t To client : ",buff);
-    bzero(buff,MAX);
-    n=0;
-
-    while((buff[n++]=getchar())!='\n');
-    write(sockfd,buff,sizeof(buff));
-
-    if(strncmp("exit",buff,4)==0){
-      printf("Server Exit...\n");
-      break;
     }
-  }
 }
 
-int main(int argc, char* argv[])
+char** str_split(char* a_str, const char a_delim)
 {
-   int nSocket, nClientSocket;
-   int nBind, nListen;
-   int nFoo = 1;
-   socklen_t nTmp;
-   struct sockaddr_in stAddr, stClientAddr;
+    char** result    = 0;
+    size_t count     = 0;
+    char* tmp        = a_str;
+    char* last_comma = 0;
+    char delim[2];
+    delim[0] = a_delim;
+    delim[1] = 0;
 
+    /* Count how many elements will be extracted. */
+    while (*tmp)
+    {
+        if (a_delim == *tmp)
+        {
+            count++;
+            last_comma = tmp;
+        }
+        tmp++;
+    }
 
-   /* address structure */
-   memset(&stAddr, 0, sizeof(struct sockaddr));
-   stAddr.sin_family = AF_INET;
-   stAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-   stAddr.sin_port = htons(SERVER_PORT);
+    /* Add space for trailing token. */
+    count += last_comma < (a_str + strlen(a_str) - 1);
 
-   /* create a socket */
-   nSocket = socket(AF_INET, SOCK_STREAM, 0);
-   if (nSocket < 0)
-   {
-       fprintf(stderr, "%s: Can't create a socket.\n", argv[0]);
-       exit(1);
-   }
-   setsockopt(nSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&nFoo, sizeof(nFoo));
+    /* Add space for terminating null string so caller
+       knows where the list of returned strings ends. */
+    count++;
 
-   /* bind a name to a socket */
-   nBind = bind(nSocket, (struct sockaddr*)&stAddr, sizeof(struct sockaddr));
-   if (nBind < 0)
-   {
-       fprintf(stderr, "%s: Can't bind a name to a socket.\n", argv[0]);
-       exit(1);
-   }
-   /* specify queue size */
-   nListen = listen(nSocket, QUEUE_SIZE);
-   if (nListen < 0)
-   {
-       fprintf(stderr, "%s: Can't set queue size.\n", argv[0]);
-   }
+    result = malloc(sizeof(char*) * count);
 
-   int rcvd;
+    if (result)
+    {
+        size_t idx  = 0;
+        char* token = strtok(a_str, delim);
 
-   while(1)
-   {
-       /* block for connection request */
-       nTmp = sizeof(struct sockaddr);
-       nClientSocket = accept(nSocket, (struct sockaddr*)&stClientAddr, &nTmp);
-       if (nClientSocket < 0)
-       {
-           fprintf(stderr, "%s: Can't create a connection's socket.\n", argv[0]);
-           exit(1);
-       }
+        while (token)
+        {
+            assert(idx < count);
+            *(result + idx++) = strdup(token);
+            token = strtok(0, delim);
+        }
+        assert(idx == count - 1);
+        *(result + idx) = 0;
+    }
 
-
-		printf("%s: [connection from %s]\n",
-		argv[0], inet_ntoa((struct in_addr)stClientAddr.sin_addr));
-
-
-		func(nClientSocket);
-		close(nSocket);
-	}
-   return(0);
+    return result;
 }
+
+char* concat(const char *s1, const char *s2)
+{
+    char *result = malloc(strlen(s1)+strlen(s2)+1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+ 
+int main(int argc , char *argv[])
+{
+    int opt = TRUE;
+    int master_socket , addrlen , new_socket , client_socket[2] , max_clients = 2 , activity, i , valread , sd, actual_clients = 0;
+    int max_sd;
+    struct sockaddr_in address;
+      
+    char buffer[1025];
+      
+
+    fd_set readfds;
+
+    char *message = "Gury multiplayer v1.0 \r\n";
+    char *full_message = "Game server is full \r\n";
+  
+    for (i = 0; i < max_clients; i++) 
+    {
+        client_socket[i] = 0;
+    }
+      
+    if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0) 
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+  
+    if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+  
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+      
+    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0) 
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Listener on port %d \n", PORT);
+     
+    if (listen(master_socket, 1) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+      
+    addrlen = sizeof(address);
+    puts("Waiting for connections ...");
+     
+    while(TRUE) 
+    {
+        FD_ZERO(&readfds);
+  
+        FD_SET(master_socket, &readfds);
+        max_sd = master_socket;
+         
+        for ( i = 0 ; i < max_clients ; i++) 
+        {
+            sd = client_socket[i];
+             
+            if(sd > 0)
+                FD_SET( sd , &readfds);
+             
+            if(sd > max_sd)
+                max_sd = sd;
+        }
+  
+        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+    
+        if ((activity < 0) && (errno!=EINTR)) 
+        {
+            printf("select error");
+        }
+          
+        if (FD_ISSET(master_socket, &readfds)) 
+        {
+            if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+            {
+                perror("accept");
+                exit(EXIT_FAILURE);
+            }
+          
+            printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+        
+            
+            if(actual_clients < max_clients){
+                if( send(new_socket, message, strlen(message), 0) != strlen(message) ) 
+                    {
+                        perror("send");
+                    }    
+            }
+            else{
+                if( send(new_socket, full_message, strlen(message), 0) != strlen(message) ) 
+                    {
+                        perror("send");
+                    }
+                close(new_socket);
+            }
+
+
+            puts("Welcome message sent successfully");
+              
+            for (i = 0; i < max_clients; i++) 
+            {
+                if( client_socket[i] == 0 )
+                {
+                    client_socket[i] = new_socket;
+                    printf("Adding to list of sockets as %d\n" , i);
+                    
+                    char * tmp = malloc(2);
+                    sprintf(tmp, "%d", i);
+                    char * s = concat("PLAYERID#", tmp);
+                    if( send(new_socket, s, strlen(s), 0) != strlen(s) ) 
+                    {
+                        perror("send");
+                    }
+                    free(tmp);
+                    free(s);
+                    actual_clients++;
+                     
+                    break;
+                }
+            }
+        }
+          
+        for (i = 0; i < max_clients; i++) 
+        {
+            sd = client_socket[i];
+              
+            if (FD_ISSET( sd , &readfds)) 
+            {
+                if ((valread = read( sd , buffer, 1024)) == 0)
+                {
+                    getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);
+                    printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+                      
+                    close( sd );
+                    client_socket[i] = 0;
+                    actual_clients--;   
+                }
+                  
+                else
+                {
+                    buffer[valread] = '\0';
+                    int k;
+                    for(k = 0; k < max_clients; k++){
+                        send(client_socket[k] , buffer , strlen(buffer) , 0 );
+                    }
+                }
+            }
+        }
+    }
+      
+    return 0;
+} 
