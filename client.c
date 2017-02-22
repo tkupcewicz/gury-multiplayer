f3324f9e40aa9d12a3f516ca1f763a35ece90b4f
Tymoteusz Kupcewicz <t.kupcewicz@gmail.com>
2017-02-22 13:29:22 +0100

    Rewritten client

--- client.c
+++ client.c
@@ -1,71 +1,86 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <arpa/inet.h>
+#include"stdio.h"
+#include"stdlib.h"
+#include"sys/types.h"
+#include"sys/socket.h"
+#include"string.h"
+#include"netinet/in.h"
+#include"pthread.h"
+#define BUF_SIZE 2000
 
-#define BUFSIZE 10000
-#define MAX 80
-char *server = "127.0.0.1";	/* adres IP pętli zwrotnej */
 char *protocol = "tcp";
-short service_port = 1234;	/* port usługi daytime */
+short service_port = 4444;  /* port usługi daytime */
 
-char bufor[BUFSIZE];
 
-void func(int sockfd)
-{
-	char buff[MAX];
-	int n;
-	for(;;){
-
-		bzero(buff,sizeof(buff));
-		printf("Enter the string : ");
-
-		n=0;
-		while((buff[n++]=getchar())!='\n');
-
-		write(sockfd,buff,sizeof(buff));
-		bzero(buff,sizeof(buff));
-		read(sockfd,buff,sizeof(buff));
-
-		printf("From Server : %s",buff);
-
-		if((strncmp(buff,"exit",4))==0){
-			printf("Client Exit...\n");
-			break;
-		}
-	}
+void * receiveMessage(void * socket) {
+    int sockfd, ret;
+    char buffer[BUF_SIZE];
+    sockfd = (int) socket;
+    memset(buffer, 0, BUF_SIZE);
+    while(ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL)){
+        //ret = recvfrom(sockfd, buffer, BUF_SIZE, 0, NULL, NULL);
+        if (ret < 0) {
+            printf("ERROR: Receiving data failed!\n");
+        } else {
+            //printf("server: ");
+            fputs(buffer, stdout);
+            memset(buffer, 0, BUF_SIZE);
+        }
+    }
 }
 
-int main ()
-{
-	struct sockaddr_in sck_addr;
+int main(int argc, char**argv) {
+    struct sockaddr_in addr;
+    int sockfd, ret;
+    char buffer[BUF_SIZE];
+    char * server;
+    pthread_t rThread;
 
-	int sck, odp;
+    if (argc < 2) {
+        printf("usage: client < ip address >\n");
+        exit(1);
+    }
+    server = argv[1];
 
-	printf ("Usługa %d na %s z serwera %s :\n", service_port, protocol, server);
+    struct sockaddr_in sck_addr;
 
-	memset (&sck_addr, 0, sizeof sck_addr);
-	sck_addr.sin_family = AF_INET;
-	inet_aton (server, &sck_addr.sin_addr);
-	sck_addr.sin_port = htons (service_port);
+    int sck, odp;
 
-	if ((sck = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
-		perror ("Nie można utworzyć gniazdka");
-		exit (EXIT_FAILURE);
-	}
+    printf ("Port: %d Protocol: %s Server: %s :\n", service_port, protocol, server);
 
-	if (connect (sck, (struct sockaddr*) &sck_addr, sizeof sck_addr) < 0) {
-		perror ("Brak połączenia");
-		exit (EXIT_FAILURE);
-	}
+    memset (&sck_addr, 0, sizeof sck_addr);
+    sck_addr.sin_family = AF_INET;
+    inet_aton (server, &sck_addr.sin_addr);
+    sck_addr.sin_port = htons (service_port);
 
-	func(sck);
-	close(sck);
+    if ((sck = socket (PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        perror ("ERROR: Creating socket failed!");
+        exit (EXIT_FAILURE);
+    }
 
-	exit (EXIT_SUCCESS);
+    if (connect (sck, (struct sockaddr*) &sck_addr, sizeof sck_addr) < 0) {
+        perror ("ERROR: Connecting failed!");
+        exit (EXIT_FAILURE);
+    }
+    printf("Connected to the server...\n");
+
+    memset(buffer, 0, BUF_SIZE);
+
+    //creating a new thread for receiving messages from the server
+    ret = pthread_create(&rThread, NULL, receiveMessage, (void *) sck);
+    if (ret) {
+        printf("ERROR: Return Code from pthread_create() is %d\n", ret);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(buffer, BUF_SIZE, stdin) != NULL) {
+        printf("buffer wyslany do servera: %s<-Koniec buffera\n", buffer);
+        ret = sendto(sck, buffer, BUF_SIZE, 0, (struct sockaddr *) &sck_addr, sizeof(sck_addr));
+        if (ret < 0) {
+            printf("ERROR: Sending data failed!\n\t-%s", buffer);
+        }
+        memset(buffer, 0, BUF_SIZE);
+    }
+
+    close(sck);
+    pthread_exit(NULL);
 }
