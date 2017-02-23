fef7fa3e4deda392137c56092bcc5643da52febd
Tymoteusz Kupcewicz <t.kupcewicz@gmail.com>
2017-02-24 00:00:31 +0100

    Server comments added

--- server.c
+++ server.c
@@ -19,6 +19,8 @@ int client_socket[MAX_CLIENTS];
 int ready_clients[MAX_CLIENTS];
 int debug_mode, seed;
 
+
+// Function takes string and splits it into tokens by delimiter 
 char** str_split(char* a_str, const char a_delim)
 {
     char** result    = 0;
@@ -67,6 +69,7 @@ char** str_split(char* a_str, const char a_delim)
     return result;
 }
 
+// Function processes message, splits, chekcs type of message and sends proper response
 void process_msg(int id, char* msg, char * response){
     strcpy(response, "\0");
     char **tokens;
@@ -74,10 +77,14 @@ void process_msg(int id, char* msg, char * response){
     char org_msg[1025];
     strcpy(org_msg, msg);
 
+    // Primitive debug to check incoming messages 
     if(debug_mode) printf("Message to split: %s\n", msg);
     tokens = str_split(msg, '#');
 
     if(debug_mode) printf("First token:%s\n", tokens[0]);
+
+
+    //Reading first token which indicates type of message
     if(strcmp(tokens[0],"$CONNECT") == 0){
         sprintf(response,"%s#%d#%d", "$CONNECTED", seed, id);
         send(client_socket[id] , response , strlen(response) , 0 );
@@ -92,6 +99,7 @@ void process_msg(int id, char* msg, char * response){
             } 
         }
     }
+
     else if(strcmp(tokens[0],"$READY") == 0){
         ready_clients[id] = 1;
         all_ready = 1;
@@ -109,15 +117,15 @@ void process_msg(int id, char* msg, char * response){
             send(client_socket[id] , response , strlen(response) , 0 );
         }
     }
+
     else if(strcmp(tokens[0],"$P") == 0){
-        // sprintf(response,"$%s", org_msg);
         for(k = 0; k < MAX_CLIENTS; k++){
             send(client_socket[k] , org_msg , strlen(org_msg) , 0 );
         }
     }
+
     else if(strcmp(tokens[0],"$DIED") == 0){
         ready_clients[id] = 0;
-        // sprintf(response,"$%s", org_msg);
         for(k = 0; k < MAX_CLIENTS; k++){
             if(client_socket[k] != 0){
                send(client_socket[k] , org_msg , strlen(org_msg) , 0 ); 
@@ -155,25 +163,28 @@ int main(int argc , char *argv[])
     for(i=1; i<argc; ++i){ 
         if(strcmp(argv[1], "debug") == 0) debug_mode = 1;
     }
-
+    // Initialise socket descriptors
     fd_set readfds;
 
     char *message = "Gury multiplayer v1.0 \r\n";
     char *full_message = "$SERVER_FULL";
     char response[1025];
   
+    // Initialise all client_socket and ready_clients to 0
     for (i = 0; i < max_clients; i++) 
     {
         client_socket[i] = 0;
         ready_clients[i] = 0;
     }
-      
+     
+    // Create master socket for the server 
     if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0) 
     {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
   
+    // Set master socket to allow multiple connections
     if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )
     {
         perror("setsockopt");
@@ -183,7 +194,8 @@ int main(int argc , char *argv[])
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
-      
+
+    // Bind master socket to localhost and port 4444  
     if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0) 
     {
         perror("bind failed");
@@ -196,17 +208,20 @@ int main(int argc , char *argv[])
         perror("listen");
         exit(EXIT_FAILURE);
     }
-      
+    
     addrlen = sizeof(address);
     puts("Waiting for connections ...");
      
     while(TRUE) 
     {
+        // Clear set of file descriptors
         FD_ZERO(&readfds);
-  
+        
+        // Add master socket to the set
         FD_SET(master_socket, &readfds);
         max_sd = master_socket;
          
+        // Add connected clients to socket set
         for ( i = 0 ; i < max_clients ; i++) 
         {
             sd = client_socket[i];
@@ -214,17 +229,20 @@ int main(int argc , char *argv[])
             if(sd > 0)
                 FD_SET( sd , &readfds);
              
+            // Highest file descriptor number for the select function
             if(sd > max_sd)
                 max_sd = sd;
         }
-  
+        
+        // Wait for an activity on one of the sockets , timeout is NULL
         activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
     
         if ((activity < 0) && (errno!=EINTR)) 
         {
             printf("select error");
         }
-          
+        
+        // Activity on master socket - handle new connection
         if (FD_ISSET(master_socket, &readfds)) 
         {
             if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
@@ -236,6 +254,7 @@ int main(int argc , char *argv[])
             printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
         
             
+            // If max clients is not reached then accept client and send welcome message, else send full server message and close socket
             if(actual_clients < max_clients){
                 if( send(new_socket, message, strlen(message), 0) != strlen(message) ) 
                     {
@@ -253,6 +272,7 @@ int main(int argc , char *argv[])
 
             puts("Welcome message sent successfully");
               
+            // Add new socket to array of client descriptors  
             for (i = 0; i < max_clients; i++) 
             {
                 if( client_socket[i] == 0 )
@@ -266,27 +286,33 @@ int main(int argc , char *argv[])
                 }
             }
         }
-          
+        
+        // If activity wasn't on master socket then check clients
         for (i = 0; i < max_clients; i++) 
         {
             sd = client_socket[i];
               
             if (FD_ISSET( sd , &readfds)) 
-            {
+            {   
+                // Read message and check if it was for closing
                 if ((valread = read( sd , buffer, 1024)) == 0)
                 {
+                    // Print details of disconnected client
                     getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);
                     printf("Player disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
                       
+                    // Close socket, decrease actual_clients number and clean up slot in arrays for reuse
                     close( sd );
                     client_socket[i] = 0;
                     actual_clients--;
                     ready_clients[i] = 0;   
                 }
-                  
+                // Process if it wasn't closing  
                 else
                 {
+                    // Set NULL byte on the end of the data read
                     buffer[valread] = '\0';
+                    // Process message
                     process_msg(i, buffer, response);
                 }
             }
